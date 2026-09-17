29a1c9eb7400c25fb1e7401b13e726a6d6952a03
chandru05092001 <chandruselva0591@gmail.com>
2026-09-17 16:29:41 +0530

    test

--- main.tf
+++ main.tf
@@ -2,6 +2,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "vulnerable_bucket" {
-  bucket = "my-test-checkov-bucket-insecure-1234"
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "my-test-checkov-bucket-secure-1234"
+}
+
+resource "aws_s3_bucket_public_access_block" "secure_bucket" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "secure_bucket" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_bucket" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
